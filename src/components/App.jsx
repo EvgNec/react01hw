eafdfd2efb0677d045e9e74d424d3800f983c555
Evgeniy <evgnec@gmail.com>
2024-04-01 20:59:23 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,11 +16,11 @@ export const App = () => {
       }}
     >
       <StatisticsList statistics={statistics} />
-{/* <ProfileCard   username={user.username}
+<ProfileCard   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
-  stats={user.stats}/> */}
+  stats={user.stats}/>
     </div>
   );
 };
