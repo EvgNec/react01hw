a0ce0655e7496d037567c42bf3aaf2c57e84ef32
Evgeniy <evgnec@gmail.com>
2024-04-22 19:29:03 +0300

    upd

---
+++ src/components/Friend/FriendCard/FriendCard.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export const FriendCard
+    = ({ avatar, name, isOnline }) =>
+    {
+        return (
+              <li>
+                  <span>{isOnline }</span>
+                  <img src={avatar} alt={name} width="48" />
+  <p>{name}</p>
+</li>
+        );
+    }
+
+
+    
+FriendCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
--- src/components/Friend/FriendList/FriendList.jsx
+++ src/components/Friend/FriendList/FriendList.jsx
@@ -1,22 +1,35 @@
+import { FriendCard } from '../FriendCard/FriendCard';
 import { StatisticsSection, Title } from '../FriendList/FriendList.styled';
+import PropTypes from 'prop-types'; 
 
 export const FriendsList = ({ friends }) => {
   return (
     <StatisticsSection>
-      <Title>Upload stats</Title>
+      <Title>FriendList</Title>
 
-      <ul class="stat-list">
+      <ul>
           {friends.map(({ id, avatar, name, isOnline }) => (
-            //   <StatisticsCard key={id} label={label} percentage={percentage} />
-              <li key={id}>
-                  <span>{isOnline }</span>
-                  <img src={avatar} alt={name} width="48" />
-  <p>{name}</p>
-</li>
+   <FriendCard key={id} avatar={avatar} name={name}  isOnline={isOnline} />
         ))} 
       </ul>
     </StatisticsSection>
   );
 };
 
+FriendsList.propTypes = {
 
+  // Перевірка, чи переданий параметр є масивом,
+  // і приймає PropTypes.shape в якості параметра.
+  cards: PropTypes.arrayOf(
+
+  // shape - визначає форму об'єкта, який передається у властивість
+  // і вимагає, щоб властивості цього об'єкта відповідали певній формі (типу даних)
+
+  PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }).isRequired
+  ).isRequired,
+};
